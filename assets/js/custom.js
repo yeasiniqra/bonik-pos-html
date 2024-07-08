073cf61878446d45c617994c78d8cc4631b9391c
yeasiniqra <yeasin@iqrasys.com>
2024-07-08 20:48:03 +0600

    testmonial

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -52,6 +52,42 @@ $(document).ready(function(){
             }
         });
     $customSlide.find(".slick-slide").eq(0).addClass("custom-slide-animation");
+
+	//testmonail JS
+	$('.swiper-wrapper').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+		autoplay: true,
+        responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+
+        ]
+      });
 	
 });
 
